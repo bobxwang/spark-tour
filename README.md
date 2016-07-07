5fd2d15e81f75846b47552e90d7fdbea11ac2cb2
王相 <wangxiang@51zhangdan.com.cn>
2016-07-07 16:13:00 +0800

    write readme

---
+++ README.md
@@ -0,0 +1,39 @@
+# spark optimize
+* serialization(default java, Kryo)
+<pre>conf.set("spark.serializer","org.apache.spark.serializer.KryoSerializer")
+conf.registerKryoClasses(Array(classOf[Myclass],classOf[Uclass]))
+</pre>
+* IO
+	* spark.local.dir 设置为多个磁盘
+	* spark.shuffle.consolidateFiles 设置为true,来合并shuffle中间文件
+	* spark.streaming.concurrentJobs
+- memory
+	* spark.memory.fraction
+	* spark.memory.storageFraction
+* other things
+	* 并行度 spark.default.parallelism
+	* 数据本地性 
+		* PROCESS_LOCAL 数据代码同一JVM进程
+		* NODE_LOCAL 数据代码同一节点
+		* NO_PREF 数据在任何地方都一样，无本地性偏好
+		* RACK_LOCAL 数据代码处于同一机架不同机器
+		* ANY 数据网络中未知，即数据代码不在同一个机架上
+	* 广播大变量，一般大小大于20KB就值得考虑
+
+# Spark Compare Map/Reduce
+* spark使用线程，而MR则是启动新的JVM
+* spark在shuffle期间仅将数据放入HDD一次，而MR需要两次
+* 传统的MR工作流是一系列job,每个job在迭代时都会将数据存入HDFS，而spark支持DAG及管道操作，这样可以让我们执行复杂的工作时不用将数据持久化
+
+# Spark Checkpoint
+一般分布式数据集的容错有**数据检查点**跟**记录数据的更新**，spark选择了后者。
+
+* Lineage机制
+
+>Lineage记录的是粗颗精确度的特定数据Transformation(map,join等),当这个RDD的部分分区数据丢失时，可通过Lineage获取足够信息来重新运算和恢复丢失的数据分区。
+
+* 窄/宽依赖
+
+> 判断依据是父RDD的每一个分区是不是会被多个子分区所依赖，是则为宽
+    
+
--- src/main/scala/com/bob/sparktour/BasicTransformation.scala
+++ src/main/scala/com/bob/sparktour/BasicTransformation.scala
@@ -2,6 +2,14 @@ package com.bob.sparktour
 
 import org.apache.spark.{SparkConf, SparkContext}
 
+case class Juice(volumn: Int) {
+  def add(juice: Juice): Juice = Juice(volumn + juice.volumn)
+}
+
+case class Fruit(king: String, weight: Int) {
+  def makeJuice: Juice = Juice(weight * 100)
+}
+
 object BasicTransformation {
 
   def main(args: Array[String]) {
@@ -51,5 +59,32 @@ object BasicTransformation {
       )
     results.collect.foreach(tpl => println(tpl._1 + " : " + tpl._2.size))
 
+    usingCombine(sc)
+  }
+
+  /**
+   * 数据分析中，处理key,value的pair数据是极为常见的场景，
+   * 从函数层面看，这类操作具有共同特征，即将类型[(K,V)]转成[(K,C)]，这里V，C可以是相同类型也可以是不同类型
+   * 这数据处理不是单纯的对pair的value进行map，而是针对不同的key值对原有的value进行联合,因而不仅类型可能不同，元素个数也不同
+   *
+   * 下面是一个具体使用例子
+   * @param sc
+   */
+  def usingCombine(sc: SparkContext): Unit = {
+    val appleone = Fruit("apple", 5)
+    val appletwo = Fruit("apple", 8)
+    val orangeone = Fruit("orange", 10)
+    val orangetwo = Fruit("orange", 20)
+
+    val f2j = (f: Fruit) => f.makeJuice
+    val fmc = (c: Juice, f: Fruit) => c.add(f.makeJuice)
+    val cmc = (c1: Juice, c2: Juice) => c1.add(c2)
+
+    val fruit = sc.parallelize(List(("apple", appleone), ("orange", orangeone), ("apple", appletwo), ("orange", orangetwo)))
+    val juice = fruit.combineByKey(f2j, fmc, cmc)
+    juice.foreach(x => {
+      println(x._1)
+      println(x._2)
+    })
   }
 }
